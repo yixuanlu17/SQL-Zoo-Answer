224bed3654a3ff20e988c0d80827d0818915a4bb
Shayla <83843271+yixuanlu17@users.noreply.github.com>
2021-05-28 16:19:11 -0500

    Update solution4.sql

--- solution/SELECT within SELECT/solution4.sql
+++ solution/SELECT within SELECT/solution4.sql
@@ -1,1 +1,25 @@
-d
+/*
+Fourth section of sqlzoo, SELECT from Nobel https://sqlzoo.net/wiki/SELECT_within_SELECT_Tutorial
+*/
+
+--#1
+/*
+List each country name where the population is larger than that of 'Russia'.
+*/
+select name from world 
+ where population > (select population from world where name = 'Russia')
+ 
+--#2
+/*
+Show the countries in Europe with a per capita GDP greater than 'United Kingdom'.
+*/ 
+select name 
+from world 
+where continent = 'Europe' and 
+GDP/population > (select GDP/population from world where name = 'United Kingdom')
+
+--#3
+/*
+List the name and continent of countries in the continents containing either Argentina or Australia. Order by name of the country.
+*/ 
+
