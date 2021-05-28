a94bac5d246e3525b953068cade2bb0e4bef4950
Shayla <83843271+yixuanlu17@users.noreply.github.com>
2021-05-28 17:09:06 -0500

    Update solution4.sql

--- solution/SELECT within SELECT/solution4.sql
+++ solution/SELECT within SELECT/solution4.sql
@@ -22,4 +22,29 @@ GDP/population > (select GDP/population from world where name = 'United Kingdom'
 /*
 List the name and continent of countries in the continents containing either Argentina or Australia. Order by name of the country.
 */ 
+select name, continent 
+from world
+where continent in (select continent from world where name in ('Argentina', 'Australia'))
+order by name
 
+--#4
+/*
+Which country has a population that is more than Canada but less than Poland? Show the name and the population.
+*/ 
+select name, population
+from world
+where population > (select population from world where name = 'Canada') and 
+population <  (select population from world where name = 'Poland')
+
+--#5
+/*
+Show the name and the population of each country in Europe. Show the population as a percentage of the population of Germany.
+*/ 
+SELECT 
+  name, 
+  CONCAT(ROUND((population*100)/(SELECT population 
+                                 FROM world WHERE name='Germany'), 0), '%')
+FROM world
+WHERE population IN (SELECT population
+                     FROM world
+                     WHERE continent='Europe')
