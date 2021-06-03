d83379ee2189c04c2ac534acf8c8cffd4d2f482c
Shayla <83843271+yixuanlu17@users.noreply.github.com>
2021-06-03 09:13:42 -0500

    Create solution5.sql

---
+++ solution/SUM and COUNT/solution5.sql
@@ -0,0 +1,3 @@
+/* Third section of sqlzoo, SELECT from Nobel https://sqlzoo.net/wiki/SELECT_from_Nobel_Tutorial */ 
+
+--#1 /* Change the query shown so that it displays Nobel prizes for 1950. */ SELECT yr, subject, winner FROM nobel WHERE yr = 1950
