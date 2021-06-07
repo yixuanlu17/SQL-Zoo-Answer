671c04a948f14bd392f8e50ae4502157ff882380
Shayla <83843271+yixuanlu17@users.noreply.github.com>
2021-06-07 14:25:11 -0500

    Update solution7.sql

--- solution/More JOIN operations/solution7.sql
+++ solution/More JOIN operations/solution7.sql
@@ -1,1 +1,63 @@
+/*
+Seventh section of sqlzoo, https://sqlzoo.net/wiki/More_JOIN_operations
+*/
 
+--#2
+/*
+When was Citizen Kane released?
+*/
+SELECT yr
+FROM movie
+WHERE title = 'Citizen Kane'
+
+--#6
+/*
+Cast list for Casablanca
+*/
+select actor.name from actor
+inner join casting  
+on actor.id = casting.actorid
+where casting.movieid = 27
+
+--#7
+/*
+Obtain the cast list for the film 'Alien'
+*/
+select actor.name from actor
+inner join casting on actor.id = casting.actorid
+inner join movie on movie.id = casting.movieid
+where movie.title = 'Alien'
+
+--#8
+/*
+List the films in which 'Harrison Ford' has appeared
+*/
+SELECT movie.title
+FROM movie
+JOIN casting ON movie.id = casting.movieid
+JOIN actor ON actor.id = casting.actorid
+WHERE name = 'Harrison Ford'
+
+--#11
+/*
+Which were the busiest years for 'Rock Hudson', show the year and the number of movies he made each year for any year in which he made more than 2 movies.
+*/
+select yr, count(movie.id) from movie
+inner join casting on movie.id = casting.movieid
+inner join actor on actor.id = casting.actorid
+where actor.name = 'Rock Hudson'
+group by yr
+having count(movie.id)>2
+
+--#12
+/*
+List the film title and the leading actor for all of the films 'Julie Andrews' played in.
+*/
+select title, name 
+from movie
+inner join casting on movie.id = casting.movieid
+inner join actor on actor.id = casting.actorid
+where movie.id in
+(select movieid from casting
+where actorid in (select id from actor where name = 'Julie Andrews'))
+and ord = 1
