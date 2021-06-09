3cffcfb4186de1fa5f48e02ac2f733dc1c4dffa7
Shayla <83843271+yixuanlu17@users.noreply.github.com>
2021-06-09 17:22:52 -0500

    Update solution10.sql

--- solution/Window functions/solution10.sql
+++ solution/Window functions/solution10.sql
@@ -1,1 +1,13 @@
+/*
+10th section of sqlzoo, https://sqlzoo.net/wiki/Window_functions
+*/
 
+--#3
+/*
+Use PARTITION to show the ranking of each party in S14000021 in each year. Include yr, party, votes and ranking (the party with the most votes is 1).
+*/
+select yr, party, votes, 
+rank()over (partition by yr order by votes desc)
+from ge
+where constituency = 'S14000024'
+order by party,yr
