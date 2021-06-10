61a947bf1ea2bb3dc67e192eae0709291fc75129
Shayla <83843271+yixuanlu17@users.noreply.github.com>
2021-06-10 17:30:18 -0500

    Update solution10.sql

--- solution/Window functions/solution10.sql
+++ solution/Window functions/solution10.sql
@@ -11,3 +11,17 @@ rank()over (partition by yr order by votes desc)
 from ge
 where constituency = 'S14000024'
 order by party,yr
+
+/*
+Covid 19 section of sqlzoo, https://sqlzoo.net/wiki/Window_LAG
+*/
+
+
+--#3
+/*
+Show the number of new cases for each day, for Italy, for March.
+*/
+SELECT name, DAY(whn) day, confirmed- LAG(confirmed,1) OVER (PARTITION BY name ORDER BY whn) newcases
+      FROM covid
+      WHERE name = 'Italy' AND MONTH(whn) = 3
+      ORDER BY whn
